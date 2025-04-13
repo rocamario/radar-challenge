8784264400ad70e3334a1c7ced17170fe40c9952
rocamario <mario.roca21@gmail.com>
2025-04-13 23:20:26 +0200

    import data from gdrive

--- src/dataset.py
+++ src/dataset.py
@@ -1,18 +1,24 @@
-import glob
 import os
-
 from user_session import UserSession
+import gdown
+import zipfile
+import streamlit as st
 
 class Dataset:
     def __init__(self, directory='data'):
         self.directory = directory
 
+        if not os.path.exists(directory):
+            print("Downloading data from Google Drive...")
+            self._download_data()
+
         self.session_ids = []
         self.sessions = {}
 
-    def load(self):
-        self.session_ids = [sid[:-5] for sid in os.listdir(self.directory)]
-        for sid in self.session_ids:
-            s = UserSession(user_id=sid, directory=self.directory)
-            s.load()
-            self.sessions[sid] = s
+    def _download_data(self):
+        file_id = st.secrets["drive"]["file_id"]
+        url = f"https://drive.google.com/uc?id={file_id}"
+        output = "data.zip"
+        gdown.download(url, output, quiet=False)
+        with zipfile.ZipFile(output, 'r') as zip_ref:
+            zip_ref.extractall(self.directory)
